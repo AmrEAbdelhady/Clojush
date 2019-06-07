8e60a1e04707a32ca5803f1db962a94d45b80ac3
Amr Abdelhady <aabdelha@hamilton.edu>
2019-06-07 11:30:31 -0400

    fixing bug in batch_tournament

--- src/clojush/pushgp/selection/batch_tournament.clj
+++ src/clojush/pushgp/selection/batch_tournament.clj
@@ -5,11 +5,10 @@
 
 (defn batch-selection
   [pop argmap]
-  (let [batch (nth (:batches argmap) @curr-batch)]
-    (if (> @curr-batch (- (count (:batches argmap)) 2))
-    (reset! curr-batch 0)
-    (swap! curr-batch + 1))
-    (let [tourn (take (:tourn-size argmap) (shuffle pop))]
+  (let [batch (nth (:batches argmap) (mod @curr-batch 6))]
+    (swap! curr-batch + 1)
 
-      (def err-vals  (map #(apply + %) (map (fn [y] (keep-indexed #(if (.contains batch %1) %2) y)) (map :errors tourn))))
-      (nth tourn (.indexOf err-vals (apply min err-vals))))))
+    (let [tourn (take (:tourn-size argmap) (shuffle pop))
+          err-vals  (map #(apply + %) (map (fn [y] (keep-indexed #(if (.contains batch %1) %2) y)) (map :errors tourn)))]
+      (def the-chosen (.indexOf err-vals (apply min err-vals)))
+      (nth tourn the-chosen))))
