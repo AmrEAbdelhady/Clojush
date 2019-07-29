f3ec60e8a9078646b67ba79abd999230c0f38b2a
Amr Abdelhady <aabdelha@hamilton.edu>
2019-07-29 20:15:46 +0200

    final implementation for combined fitness

--- src/clojush/pushgp/selection/combined_fitness.clj
+++ src/clojush/pushgp/selection/combined_fitness.clj
@@ -1,6 +1,9 @@
 (ns clojush.pushgp.selection.combined-fitness
   (:use [clojush random]))
 
+  (defn binary-errors
+    [evaluated-population argmap ind]
+    (count (filter #(= 0 %) (:error ((:error-function argmap) ind :train)))))
 
 (defn combined-fitness-selection
   "Returns an individual that does the best out of a tournament."
@@ -12,17 +15,9 @@
                  (:hah :rmse :ifs) :weighted-error
                  (throw (Exception. (str "Unrecognized argument for total-error-method: "
                                          total-error-method))))
-        zero-err-seq    (map (:meta-error argmap) tournament-set) ;;anon function??
+        zero-err-seq    (map (partial binary-errors pop argmap) tournament-set)
         max-err (err-fn (apply max-key err-fn tournament-set))
-        ; max-zero-errs (apply max zero-err-seq)
-        weight-factor (/ max-err (count (:training-cases argmap)))  ;; training-cases: did i just implement this for certain methods or was it there?
+        weight-factor (/ max-err (count (:training-cases argmap)))
         weighted-zero-seq (map #(* weight-factor %) zero-err-seq)]
 
-    (.indexOf tournament-set (apply min (map #(- %1 %2) (map err-fn tournament-set) weighted-zero-seq)))))
-    ; (apply min-key err-fn tournament-set)))
-    ; (apply min-key #(err-fn % argmap tournament-set) tournament-set)))
-
-; (defn combined-err-fn
-;   [ind argmap]
-;
-;   )
+    (nth tournament-set (.indexOf (map #(- %1 %2) (map err-fn tournament-set) weighted-zero-seq) (apply min (map #(- %1 %2) (map err-fn tournament-set) weighted-zero-seq))))))
